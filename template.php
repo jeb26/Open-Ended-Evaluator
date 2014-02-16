4adab4c87e3219f02623dc763d28c9d70dc52a68
James Edward Bell <bell.edward.james@gmail.com>
2014-02-16 03:43:53 -0500

    added textarea to collect programming input

---
+++ template.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Open Ended Response in Java</title></head>
+
+<body>
+<p>Write a function that will add two numbers:<br /><br />addTwo(intA, intB) --> intA + intB<br /><br /></p>
+
+<form>
+	<textarea rows="25" cols="100"></textarea>
+</form>
+
+</body>
+
+</html>
